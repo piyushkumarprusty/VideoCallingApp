d62803410db238332bf8ef420cb389fe3c9d8e3e
Piyush Kumar Prusty <piyushprusty10@gmail.com>
2021-06-27 01:59:30 +0530

    firebase validation while SignUp

--- app/src/main/java/com/example/android/videocallingapp/MainActivity.java
+++ app/src/main/java/com/example/android/videocallingapp/MainActivity.java
@@ -5,13 +5,19 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+
 
         /*  This is for progress bar    */
 
--- app/src/main/java/com/example/android/videocallingapp/SignupActivity.java
+++ app/src/main/java/com/example/android/videocallingapp/SignupActivity.java
@@ -1,14 +1,60 @@
 package com.example.android.videocallingapp;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class SignupActivity extends AppCompatActivity {
+    FirebaseAuth auth;
+    EditText emailBox, passwordBox,nameBox;
+    Button loginBtn, signupBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_signup);
+
+        auth = FirebaseAuth.getInstance();  //it create an instance with firebase and user create an new account this
+        emailBox = findViewById(R.id.emailBox);
+        nameBox = findViewById(R.id.nameBox);
+        passwordBox = findViewById(R.id.passwordBox);
+
+        loginBtn = findViewById(R.id.logintbtn);
+        signupBtn = findViewById(R.id.createbtn);
+
+        signupBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String email ,name  ,pass ;
+                email = emailBox.getText().toString();
+                name = nameBox.getText().toString();
+                pass = passwordBox.getText().toString();
+
+                auth.createUserWithEmailAndPassword(email,pass).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+
+                        if (task.isSuccessful()){
+                            // success
+
+                        }
+                        else {
+                            Toast.makeText(SignupActivity.this,task.getException().getLocalizedMessage(),Toast.LENGTH_SHORT).show();
+                        }
+
+                    }
+                });
+            }
+        });
     }
 }
